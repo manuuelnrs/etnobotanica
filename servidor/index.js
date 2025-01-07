f4add29c13d5176e122e74d8533aaeba390ad2ef
manuuelnrs <juan.nava@fi.unam.edu>
2025-01-06 21:44:13 -0600

    update devDependencies

--- servidor/index.js
+++ servidor/index.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const cors = require('cors')
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(
   cors({
